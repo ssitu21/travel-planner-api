151fcf38c0483b4137ce0aa5072cf53ac7e8949b
ssitu21 <shawns097@deltalearns.ca>
2026-06-01 09:13:26 -0700

    finished my 1st api code area
    I added the countries information's provided  for what the users has entered for the country

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import requests
 country = input("Enter the country name:").strip()
 
 # Generate the URL
-URL = f"https://restcountries.com/v3.1/name/{country}?fullText=true"  
+URL = f"https://restcountries.com/v3.1/name/{country}"  
 
 # Use Requests to get data from the URL back in JSON format
 # JSON = javascript object notation (you can also use XML but I prefer JSON)
@@ -17,3 +17,20 @@ jsondata = requests.get(URL).json()
 # Get the country returned
 data = jsondata[0]
 
+# Use brackets just like in lists and dictionaries to get the exact data the user needs
+country_name = data["name"]["common"]
+capital = data["capital"][0]
+population = data["population"]
+region = data["region"]
+currency = list(data["currencies"].keys())[0]
+language = list(data["languages"].values())[0]
+
+# Print the information
+print("Country Information: ")
+print()
+print(f"Country:    {country_name}")
+print(f"Capital:    {capital}")
+print(f"Population: {population:,}")
+print(f"Region:     {region}")
+print(f"Currency:   {currency}")
+print(f"Language:   {language}")
